10e8e04e04dad123baf16d33517bd9effae886e6
Alex Protyagov <protyagov@gmail.com>
2022-06-22 05:05:13 -0500

    Updates

--- src/img/img.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FILES=*.png
-for f in $FILES
-do
-  cwebp -lossless -z 9 -m 6 -mt -hint picture -metadata none $f -o $f.webp
-done
-
-ls *webp | sed 's/^\(.*\)\.png.webp$/mv "\1.png.webp" "\1.webp"/' | sh
