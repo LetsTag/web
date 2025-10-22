6b592bbe94f1150d2ef0ceed3b23a1fcbc8d73b6
Alex Protyagov <protyagov@gmail.com>
2025-10-21 23:41:02 -0500

    Add pick of somatotypes

--- webpack.com.js
+++ webpack.com.js
@@ -57,6 +57,7 @@ module.exports = {
                 { context: 'src/img/emotion/', from: '*.webp', to: "img" },
                 { context: 'src/img/emotion_watch/', from: '*.png', to: "img" },
                 { context: 'src/img/size/', from: '*.webp', to: "img" },
+                { context: 'src/img/size/somato/male', from: '*.webp', to: "img/somato/male" },
                 { context: 'src/img/size_watch/', from: '*.png', to: "img" },
                 { context: 'src/img/icon/', from: '*', to: "icon" },
             ],
